74b928bc1c6b123cc796cd74e3602663a76a847e
Bashar Wadan <Basharwadan@gmail.com>
2021-11-01 20:57:22 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,11 @@
     > Motors Drivers Are ( 2 * BTS7960 )
 */
 
+// IF White Line define WHITE
+// IF Black Line define BLACK
+#define WHITE
+/////////////////////////////
+
 
 #include <Arduino.h>
 
@@ -16,17 +21,26 @@ int PWMB_RIGHT = 8;
 int PWMA_LEFT = 7;
 int PWMB_LEFT = 6;
 
-#define IR_R A1
-#define IR_L 10
+#define IR_R 3
+#define IR_L 4
 
-int Right, Left;
+//Spedd of The Robot
+#define spd 70
+////////////////////
 
+
+//All Possible States The System Can present
 #define Center 0
 #define Right 1
 #define Left 2
+#define Stop 3
+/////////////////////////////////////////////
 
-#define spd 70
-
+// define a state type that can present one of the previousely defined
+// each state shoud have
+//   > output
+//   > Time (delay)
+// each state shoud refer to the next state based on the input data
 typedef struct 
 {
   int out;
@@ -34,23 +48,50 @@ typedef struct
   int next[4];
 } State_t;
 
-const State_t fsm[3] =
-{
-  {0x03, 10, {Center, Left, Right, Center}},
-  {0x02, 10, {Center, Left, Center, Right}},
-  {0x01, 10, {Center, Center, Right, Left}}
-};
+// Define the State transition table
+#ifdef WHITE
+  const State_t fsm[4] =
+  {
+
+        {0x03, 10, {Center,    Right,     Left,      Center}}, // Center 0
+        {0x02, 10, {Center,    Center,    Left,      Right}}, // Right 1
+        {0x01, 10, {Center,    Right,     Center,    Left}}, // Left 2
+        {0x00, 10, {Center,    Right,     Left,      Stop}} // Stop 3
+
+  };
+
+#elif BLACK
+  const State_t fsm[4] =
+  {
+
+        {0x03, 10, {Stop,      Left,      Right,       Center}}, // Center 0
+        {0x02, 10, {Right,     Left,      Center,      Center}}, // Right 1
+        {0x01, 10, {Left,      Center,    Right,       Center}}, // Left 2
+        {0x00, 10, {Stop,      Left,      Right,       Center}} // Stop 3
+
+  };
+#endif
+////////////////////////////////////////////////////////////////////////////////
 
 
-void DriveMotors(int st);
-int ReadSensors();
+/////////// Function Declarations /////////////
+void DriveMotors(int st); // Control the motors
+int ReadSensors(); // Read the two IRs
+//////////////////////////////////////////////
+
 
 int state;
 int input;
 int output;
+
 void setup() {
   // put your setup code here, to run once:
   state = Center;
+
+  pinMode(IR_R, INPUT);
+  pinMode(IR_L, INPUT);
+
+  Serial.begin(9600);
 
 }
 
@@ -69,14 +110,14 @@ int ReadSensors()
   int right = digitalRead(IR_R);
   int left = digitalRead(IR_L);
   // return rightleft adjacent bits
-  return ( (right << 1) | (left) );
+  return ( (right) + (left<< 1) );
 }
 
 void DriveMotors(int out)
 {
-  out = ~out;
-  int right = out & 0x02; // second bit in output 
-  int left = out & 0x01;  // first bit in output
+
+  int right = out & 0x01; // first bit in output 
+  int left = out & 0x02;  // second bit in output
 
   if ( right && left)
   {
