03cca67a70c1e151935f7fa15067a0763d223117
Bashar Wadan <Basharwadan@gmail.com>
2021-11-15 12:48:19 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -48,26 +48,63 @@ typedef struct
   int next[4];
 } State_t;
 
+
 // Define the State transition table
+
+// Case WHITE
+//               Possible Inputs
+// 00  =>  On Line              State: Center             Out: 3 (11)2
+// 01  =>  Off To The Right     State: Right              Out: 1 (01)2
+// 10  =>  Off To The Left      State: Left               Out: 2 (10)2
+// 11  =>  Off Line             State: Stop               Out: 0 (00)2
+
+
+// Possible outputs
+/*
+    left motor stop      <----     ----> right motor stop
+                              |    |
+                              0    0
+                              
+
+    left motor stop      <----     ----> right motor move
+                              |    |
+                              0    1
+                              
+    left motor move      <----     ----> right motor stop
+                              |    |
+                              1    0
+                              
+    left motor move      <----     ----> right motor move
+                              |    |
+                              1    1                                                
+*/
+
 #ifdef WHITE
   const State_t fsm[4] =
   {
 
-        {0x03, 10, {Center,    Right,     Left,      Center}}, // Center 0
-        {0x02, 10, {Center,    Center,    Left,      Right}}, // Right 1
-        {0x01, 10, {Center,    Right,     Center,    Left}}, // Left 2
-        {0x00, 10, {Center,    Right,     Left,      Stop}} // Stop 3
+        {0x03, 10, {Center,    Right,     Left,      Center}}, // Center 0  0x03 => go straight
+        {0x01, 10, {Center,    Center,    Left,      Right}}, // Right 1    0x01 => turn left
+        {0x02, 10, {Center,    Right,     Center,    Left}}, // Left 2      0x02 => turn right
+        {0x00, 10, {Center,    Right,     Left,      Stop}} // Stop 3       0x00 => stop
 
   };
+
+// Case BLACK
+//               Possible Inputs
+// 11  =>  On Line              State: Center             Out: 3 (11)2
+// 10  =>  Off To The Right     State: Right              Out: 1 (01)2
+// 01  =>  Off To The Left      State: Left               Out: 2 (10)2
+// 00  =>  Off Line             State: Stop               Out: 0 (00)2
 
 #elif BLACK
   const State_t fsm[4] =
   {
 
-        {0x03, 10, {Stop,      Left,      Right,       Center}}, // Center 0
-        {0x02, 10, {Right,     Left,      Center,      Center}}, // Right 1
-        {0x01, 10, {Left,      Center,    Right,       Center}}, // Left 2
-        {0x00, 10, {Stop,      Left,      Right,       Center}} // Stop 3
+        {0x03, 10, {Stop,      Left,      Right,       Center}},   // Center 0  0x03 => go straight
+        {0x01, 10, {Right,     Left,      Center,      Center}},  // Right 1    0x01 => turn left
+        {0x02, 10, {Left,      Center,    Right,       Center}}, // Left 2      0x02 => turn right
+        {0x00, 10, {Stop,      Left,      Right,       Center}} // Stop 3       0x00 => stop
 
   };
 #endif
@@ -95,13 +132,25 @@ void setup() {
 
 }
 
+int possible_input[] = {
+    0, 1, 2, 3
+};
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  output = fsm[state].out;
-  DriveMotors(output);
-  delay(fsm[state].time);
-  input = ReadSensors();
-  state = fsm[state].next[input];
+
+  for (int i=0; i<=3; i++)
+  {
+    // put your main code here, to run repeatedly:
+    output = fsm[state].out;
+    //Serial.print("state: "); Serial.print(state);
+    //Serial.print("  out: "); Serial.print(output);
+    DriveMotors(output);
+    delay(fsm[state].time);
+    input = ReadSensors();
+    state = fsm[state].next[input];
+    //Serial.print("  in: "); Serial.print(possible_input[i]);
+    //Serial.print("  new state: "); Serial.println(state);
+  }
 
 }
 
@@ -109,13 +158,12 @@ int ReadSensors()
 {
   int right = digitalRead(IR_R);
   int left = digitalRead(IR_L);
-  // return rightleft adjacent bits
+  // return left-right adjacent bits
   return ( (right) + (left<< 1) );
 }
 
 void DriveMotors(int out)
 {
-
   int right = out & 0x01; // first bit in output 
   int left = out & 0x02;  // second bit in output
 
@@ -127,6 +175,8 @@ void DriveMotors(int out)
     // left on forward
     analogWrite(PWMA_LEFT, spd);
     analogWrite(PWMB_LEFT, 0);
+
+    //Serial.print(" Straigt");
   }
   else if (right)
   {
@@ -136,6 +186,8 @@ void DriveMotors(int out)
     // left off 
     analogWrite(PWMA_LEFT, 0);
     analogWrite(PWMB_LEFT, 0);
+    //Serial.print(" go left");
+
   }
   else if (left)
   {
@@ -145,6 +197,8 @@ void DriveMotors(int out)
     // right off
     analogWrite(PWMA_RIGHT, 0);
     analogWrite(PWMB_RIGHT, 0);
+    //Serial.print(" go right");
+
   }
   else 
   {
@@ -155,5 +209,7 @@ void DriveMotors(int out)
     // right off
     analogWrite(PWMA_RIGHT, 0);
     analogWrite(PWMB_RIGHT, 0);
+    //Serial.print(" stop");
+
   }
 }
